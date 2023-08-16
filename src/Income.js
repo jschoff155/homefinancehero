ee3e45f1db56c12204b09fe1b84f8467503907ec
Jacob Schoff <jschoff155@gmail.com>
2023-08-16 17:08:32 -0500

    Revised more content on the income slide.

--- src/Income.js
+++ src/Income.js
@@ -2,19 +2,60 @@ import React from 'react'
 import './StyleSheets/Income.css'
 
 export default function Income() {
+    function hourlyPayQs(){
+        document.getElementById("hourlySelected").style.display="block";
+        document.getElementById("monthlySelected").style.display="none";
+        document.getElementById("salarySelected").style.display="none";
+    }
+    function monthlyPayQs(){
+        document.getElementById("hourlySelected").style.display="none";
+        document.getElementById("monthlySelected").style.display="block";
+        document.getElementById("salarySelected").style.display="none";
+    }
+    function salaryPayQs(){
+        document.getElementById("hourlySelected").style.display="none";
+        document.getElementById("monthlySelected").style.display="none";
+        document.getElementById("salarySelected").style.display="block";
+    }
+    
+    //let hourlyPay = parseInt(document.getElementById("hourlyPay").value);
+   // let monthlyPay = parseInt(document.getElementById("monthlyPay").value);
+   // let salaryPay = parseInt(document.getElementById("hourlyPay").value);
+
+   // function monthlyIncomeH(){
+        
+    //}
+    
+   // function monthlyIncomeS(){}
+
   return (
     <div>
         <h1>Income</h1>
         <h2>Borrower One</h2>
         <label for = "payType">How are you paid?</label>
         <select id="payType">
-            <option value="hourly">Hourly</option>
-            <option value="monthly">Monthly</option>
-            <option value="salary">Salary</option>
+            <option>Please Select</option>
+            <option value="hourly" onClick={hourlyPayQs}>Hourly</option>
+            <option value="monthly" onClick={monthlyPayQs}>Monthly</option>
+            <option value="salary" onClick={salaryPayQs}>Salary</option>
         </select>
-        <label id="hourlySelected">How much do you make per hour?</label>
-        <label id="monthlySelected">How much do you make per month?</label>
-        <label id="salarySelected">How much do you make per year?</label>
+        <div id="hourlySelected">
+            <label>How much do you make per hour?</label>
+            <input id="hourlyPay"></input>
+            <label>Your gross income per month:</label>
+        </div>
+        <div id="monthlySelected">
+            <label>How much do you make per month?</label>
+            <input id="monthlyPay"></input>
+            <label>Your gross income per month:</label>
+            <label id="grossMonthly"></label>
+        </div>
+        <div id="salarySelected">
+            <label>How much do you make per year?</label>
+            <input id="annualPay"></input>
+            <label>Your gross income per month:</label>
+        </div>
+        
     </div>
   )
 }
