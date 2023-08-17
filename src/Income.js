5f19203fa9a428712a7846720662fb0a3ac702d6
Jacob Schoff <jschoff155@gmail.com>
2023-08-17 00:02:52 -0500

    Created variable handlers for variables. Functions for hourly and monthly work correctly. Revising function and math for salary input next.

--- src/Income.js
+++ src/Income.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './StyleSheets/Income.css'
 
 export default function Income() {
+    const [hourlyPay, sethourlyPay] = useState("");
+    const [monthlyPay, setmonthlyPay] = useState("");
+    const [salaryPay, setsalaryPay] = useState("");
+    
+    const onChangeH = event => sethourlyPay(event.target.value);
+    const onChangeM = event => setmonthlyPay(event.target.value);
+    const onChangeS = event => setsalaryPay(event.target.value);
+
     function hourlyPayQs(){
         document.getElementById("hourlySelected").style.display="block";
         document.getElementById("monthlySelected").style.display="none";
@@ -17,19 +25,21 @@ export default function Income() {
         document.getElementById("monthlySelected").style.display="none";
         document.getElementById("salarySelected").style.display="block";
     }
-    
-    //let hourlyPay = parseInt(document.getElementById("hourlyPay").value);
-   // let monthlyPay = parseInt(document.getElementById("monthlyPay").value);
-   // let salaryPay = parseInt(document.getElementById("hourlyPay").value);
-
-   // function monthlyIncomeH(){
-        
-    //}
-    
-   // function monthlyIncomeS(){}
+    function monthlyIncomeH(){
+        let mGrossH = ((parseInt(hourlyPay) * 2080)/12);
+        document.getElementById("mGrossH").innerHTML = `${mGrossH}`;
+    }
+    function monthlyIncomeM(){
+        let mGrossM = (parseInt(monthlyPay));
+        document.getElementById("mGrossM").innerHTML = `${mGrossM}`;
+    }
+    function monthlyIncomeS(){
+        let mGrossS = (parseInt(salaryPay)/12);
+        document.getElementById("mGrossS").innerHTML = `${mGrossS}`;
+    }
 
   return (
-    <div>
+    <div id="income">
         <h1>Income</h1>
         <h2>Borrower One</h2>
         <label for = "payType">How are you paid?</label>
@@ -41,21 +51,27 @@ export default function Income() {
         </select>
         <div id="hourlySelected">
             <label>How much do you make per hour?</label>
-            <input id="hourlyPay"></input>
-            <label>Your gross income per month:</label>
+            <input type="text" value={hourlyPay} onChange={onChangeH}></input>
+            <button onClick={monthlyIncomeH}>Calculate monthly</button>
+            <label>Your gross income per month: $</label>
+            <label id="mGrossH"></label>
         </div>
+
         <div id="monthlySelected">
             <label>How much do you make per month?</label>
-            <input id="monthlyPay"></input>
-            <label>Your gross income per month:</label>
-            <label id="grossMonthly"></label>
+            <input type="text" value={monthlyPay} onChange={onChangeM}></input>
+            <button onClick={monthlyIncomeM}>Calculate monthly</button>
+            <label>Your gross income per month: $</label>
+            <label id="mGrossM"></label>
         </div>
+
         <div id="salarySelected">
             <label>How much do you make per year?</label>
-            <input id="annualPay"></input>
-            <label>Your gross income per month:</label>
+            <input type="text" value={salaryPay} onChange={onChangeS}></input>
+            <button onClick={monthlyIncomeS}>Calculate monthly</button>
+            <label>Your gross income per month: $</label>
+            <label id="MgrossS"></label>
         </div>
-        
     </div>
   )
 }
