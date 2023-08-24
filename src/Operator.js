c8288821c3ba385fddefdc68aa326b7e4eabcb04
Jacob Schoff <jschoff155@gmail.com>
2023-08-24 11:19:53 -0500

    Working on props and passing values

--- src/Operator.js
+++ src/Operator.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Statusbar from './Statusbar';
+import Propertyinfo from './Propertyinfo';
+import MonthlyExpenses from './MonthlyExpenses';
+import Income from './Income';
+import Assets from './Assets';
 
 export default function Operator(props) {
-    const tlAmount = props.tLoanAmount
-
+  const tAOTF = (props.tAssets);
   return (
     <div>
         <p></p>
-      <Statusbar tLAmount={tlAmount}/>
+        <Statusbar assetsOnFile={tAOTF}/>
     </div>
   )
 }
