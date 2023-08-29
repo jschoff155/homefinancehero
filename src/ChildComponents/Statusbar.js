70856b31894e543226b3198ccfb0f6db856244f7
Jacob Schoff <jschoff155@gmail.com>
2023-08-29 10:00:18 -0500

    See previous notes

--- src/ChildComponents/Statusbar.js
+++ src/ChildComponents/Statusbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './StyleSheets/statusbar.css'
 
 export default function Statusbar(props) {
-
+  
   return (
     <div className = "statusBar">
       <div className = "statusItem"><p>Debt-To-Income:</p> {}</div>
