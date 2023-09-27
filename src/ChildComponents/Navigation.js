6a1314b4f7ab90c85c9f8c0bfb7e191d3a7fcffc
Jacob Schoff <jschoff155@gmail.com>
2023-09-27 11:38:22 -0500

    Removed unneeded assets, added icons, updated navigation.

--- src/ChildComponents/Navigation.js
+++ src/ChildComponents/Navigation.js
@@ -3,7 +3,12 @@ import '../StyleSheets/Navigation.css'
 
 export default function Navigation() {
   return (
+    
     <div id="navigation">
+    <div className="Icons">
+    <img src="../menu (1).png" alt="menuIcon"></img>
+    <img src="../close (1).png" alt="closeIcon"></img>
+    </div>
       <button class="navButton">Property</button>
       <button class="navButton">Income</button>
       <button class="navButton">Expenses</button>
