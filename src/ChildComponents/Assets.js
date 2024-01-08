40d43362b30e4400514f958ba41655a5954ecfda
Jacob Schoff <jschoff155@gmail.com>
2024-01-07 20:45:08 -0600

    Successfully setup props from components to statusbar for assets and loan amount.

--- src/ChildComponents/Assets.js
+++ src/ChildComponents/Assets.js
@@ -1,45 +1,67 @@
-import React, { useState } from 'react'
-import '../StyleSheets/Assets.css'
+import React, { useState, useEffect } from "react";
+import "../StyleSheets/Assets.css";
 
-export default function Assets() {
-    const [cAAmount, setcAAmount] = useState("");
-    const [sAAmount, setsAAmount] = useState("");
-    const [rEAmount, setrEAmount] = useState("");
-    const [gAmount, setgAmount] = useState("");
-    
-    const onChangeCHK = event => setcAAmount(event.target.value);
-    const onChangeSAV = event => setsAAmount(event.target.value);
-    const onChangeRET = event => setrEAmount(event.target.value);
-    const onChangeGFT = event => setgAmount(event.target.value);
+export default function AssetsArea({ onChangeAssetTotal }) {
+  const [cAAmount, setcAAmount] = useState("");
+  const [sAAmount, setsAAmount] = useState("");
+  const [rEAmount, setrEAmount] = useState("");
+  const [gAmount, setgAmount] = useState("");
 
-    const totalAssets = (cAAmount && sAAmount && rEAmount && gAmount && (parseInt(cAAmount)+parseInt(sAAmount)+parseInt(rEAmount)+parseInt(gAmount)));
+  const onChangeCHK = (event) => setcAAmount(event.target.value);
+  const onChangeSAV = (event) => setsAAmount(event.target.value);
+  const onChangeRET = (event) => setrEAmount(event.target.value);
+  const onChangeGFT = (event) => setgAmount(event.target.value);
 
-    return (
+  const totalAssets =
+    cAAmount &&
+    sAAmount &&
+    rEAmount &&
+    gAmount &&
+    parseInt(cAAmount) +
+      parseInt(sAAmount) +
+      parseInt(rEAmount) +
+      parseInt(gAmount);
+
+  useEffect(() => {
+    onChangeAssetTotal(totalAssets);
+  });
+
+  return (
     <div id="assets">
       <h1>Assets on file</h1>
-      <label>Total Assets on file: ${totalAssets && (parseInt(totalAssets)).toFixed(2)}</label>
-      <table className='aTable'>
-            <tr className="atr">
-              <th className="aTh">Asset Type</th>
-              <th className="aTh">Total Amount</th>
-            </tr>
-            <tr className="atr">
-              <td class="assetType">Checking Accounts</td>
-              <td><input type="text" value={cAAmount} onChange={onChangeCHK}></input></td>
-            </tr>
-            <tr className="atr">
-              <td class="aLabel">Savings Accounts</td>
-              <td class="aInputField"><input type="text" value={sAAmount} onChange={onChangeSAV}></input></td>
-            </tr>
-            <tr className="atr">
-              <td class="aLabel">IRAs/401Ks</td>
-              <td class="aInputField"><input type="text" value={rEAmount} onChange={onChangeRET}></input></td>
-            </tr>
-            <tr className="atr">
-              <td class="aLabel">Gifts</td>
-              <td class="aInputField"><input type="text" value={gAmount} onChange={onChangeGFT}></input></td>
-            </tr>
-        </table>
+      <label>
+        Total Assets on file: ${totalAssets && parseInt(totalAssets).toFixed(2)}
+      </label>
+      <table className="aTable">
+        <tr className="atr">
+          <th className="aTh">Asset Type</th>
+          <th className="aTh">Total Amount</th>
+        </tr>
+        <tr className="atr">
+          <td class="assetType">Checking Accounts</td>
+          <td>
+            <input type="text" value={cAAmount} onChange={onChangeCHK}></input>
+          </td>
+        </tr>
+        <tr className="atr">
+          <td class="aLabel">Savings Accounts</td>
+          <td class="aInputField">
+            <input type="text" value={sAAmount} onChange={onChangeSAV}></input>
+          </td>
+        </tr>
+        <tr className="atr">
+          <td class="aLabel">IRAs/401Ks</td>
+          <td class="aInputField">
+            <input type="text" value={rEAmount} onChange={onChangeRET}></input>
+          </td>
+        </tr>
+        <tr className="atr">
+          <td class="aLabel">Gifts</td>
+          <td class="aInputField">
+            <input type="text" value={gAmount} onChange={onChangeGFT}></input>
+          </td>
+        </tr>
+      </table>
     </div>
-  )
+  );
 }
--- src/ChildComponents/Propertyinfo.js
+++ src/ChildComponents/Propertyinfo.js
@@ -1,30 +1,38 @@
-import React, { useState } from 'react'
-import '../StyleSheets/Propertyinfo.css'
+import React, { useState, useEffect } from "react";
+import "../StyleSheets/Propertyinfo.css";
 
-export default function Propertyinfo() {
-  
+export default function Propertyinfo({ onChangeLoanTotal }) {
   const [homePrice, setHomePrice] = useState("");
   const [downPayment, setDownPayment] = useState("");
 
-  const tLoanAmount = (homePrice && downPayment && (parseInt(homePrice) - parseInt(downPayment)));
-  
-  const onChangeHP = event => setHomePrice(event.target.value);
-  const onChangeDP = event => setDownPayment(event.target.value);
-  
-    return (
+  const tLoanAmount =
+    homePrice && downPayment && parseInt(homePrice) - parseInt(downPayment);
+
+  const onChangeHP = (event) => setHomePrice(event.target.value);
+  const onChangeDP = (event) => setDownPayment(event.target.value);
+
+  useEffect(() => {
+    onChangeLoanTotal(tLoanAmount);
+  });
+
+  return (
     <div id="propertyInformation">
       <form id="pIform">
         <h1>Property Information</h1>
         <label>Total Loan Amount: $</label>
-        <label id="loanAmount">{tLoanAmount && ((parseInt(tLoanAmount))).toFixed(2)}</label>
+        <label id="loanAmount">
+          {tLoanAmount && parseInt(tLoanAmount).toFixed(2)}
+        </label>
         <br></br>
-        <label>Home Price</label><br></br>
-        <input type="text" value={homePrice} onChange={onChangeHP}></input><br></br>
-        <label>Down Payment</label><br></br>
-        <input type="text" value={downPayment} onChange={onChangeDP}></input><br></br>
-        
-        
+        <label>Home Price</label>
+        <br></br>
+        <input type="text" value={homePrice} onChange={onChangeHP}></input>
+        <br></br>
+        <label>Down Payment</label>
+        <br></br>
+        <input type="text" value={downPayment} onChange={onChangeDP}></input>
+        <br></br>
       </form>
     </div>
-  )
+  );
 }
