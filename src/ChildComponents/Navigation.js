720456c585491181065344720cbfdffe23a9f876
Jacob Schoff <jschoff155@gmail.com>
2024-01-09 15:37:55 -0600

    Updated buttons with MUI design.

--- src/ChildComponents/Navigation.js
+++ src/ChildComponents/Navigation.js
@@ -1,15 +1,16 @@
 import React from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 import "../StyleSheets/Navigation.css";
 
 export default function Navigation() {
   return (
-    <div id="navigation">
-      <div className="Icons"></div>
-      <button class="navButton">Property</button>
-      <button class="navButton">Income</button>
-      <button class="navButton">Expenses</button>
-      <button class="navButton">Assets</button>
-      <button class="navButton">Financing</button>
-    </div>
+    <Stack spacing={2} direction="row">
+      <Button variant="outlined">Property</Button>
+      <Button variant="outlined">Income</Button>
+      <Button variant="outlined">Expenses</Button>
+      <Button variant="outlined">Assets</Button>
+      <Button variant="outlined">Financing</Button>
+    </Stack>
   );
 }
